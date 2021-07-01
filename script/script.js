4cef06483d89b6adf34d04e918c2ece104793018
Nhan <vinh.yan93@gmail.com>
2021-07-01 14:15:25 -0400

    rewrite code in JS

--- script/script.js
+++ script/script.js
@@ -1,49 +1,52 @@
 const form = document.querySelector('form')
 const submitBtn = document.querySelector('button')
-const errMsgDivs = document.querySelectorAll('.err-msg')
-const nameAndPwInputs = document.querySelectorAll('input[type=text], input[type=password]')
-const emailInput = document.querySelector('input[type=email]')
-const passwordInput = document.querySelector('input[type=password]')
+const inputs = document.querySelectorAll('input');
 
+
+// Display error message:
+const errInput = (input, errMsg, errIcon) => {
+
+    input.type == 'email' ? errMsg.innerText = `Looks like this is not an email` : errMsg.innerText = `${input.placeholder} cannot be empty`;
+
+    input.classList.add('error-input')
+
+    errIcon.classList.add('error-icon')
+
+}
 
 
 const app = () => {
 
      form.addEventListener('submit', (e) => {
 
-        nameAndPwInputs.forEach(input => {
-            if (input.value == null || input.value == '') {
-                input.classList.add('none-text')
-                const errMsgDiv = input.nextElementSibling; 
-                errMsgDiv.innerText = `${input.placeholder} cannot be empty`
+        inputs.forEach(input => {
 
-                input.style.border = '1px solid hsl(0, 100%, 74%)';
+            const errIcon = input.closest('.input-container').querySelector('img');
 
-                const errorIcon = input.closest('.input-container').querySelector('img');
-                errorIcon.classList.add('error-on')
-                
+            const errMsg = input.nextElementSibling;
 
+            if (input.type == 'email' && !input.value.includes('@')) {
+                input.classList.add('red-text');
+                input.placeholder = 'email@example.com'
+                errInput(input, errMsg, errIcon);
                 e.preventDefault();
 
+            } else if (input.value == null || input.value == '') {
+                input.classList.add('none-text')
+                errInput(input, errMsg, errIcon);
+                
+                e.preventDefault();
+                
+        
+            } else {
+                input.classList.remove('error-input')
+                errIcon.classList.remove('error-icon')
+                errMsg.innerText = ''
             }
         })
-
-        if (!emailInput.value.includes('@')) {
-            emailInput.classList.add('red-text');
-            emailInput.placeholder = 'email@example.com'
-            const errMsgDiv = emailInput.nextElementSibling;
-            errMsgDiv.innerText = `Looks like this is not an email`
-
-            emailInput.style.border = '1px solid hsl(0, 100%, 74%)';
-
-            const errorIcon = emailInput.closest('.input-container').querySelector('img');
-            errorIcon.classList.add('error-on')
-            e.preventDefault();
-        }
+       
 
      })
-
-
 
 
 }
